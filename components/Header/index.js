5724ebd1a13977e552eb09b58dc619123c68ff6f
Kira <kawoods826@gmail.com>
2019-09-17 12:19:10 -0700

    added component for header to index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,33 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header() {
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = 'MARCH 28, 2019';
+    header.appendChild(date);
+
+    const title = document.createElement('h2');
+    title.textContent = 'Lambda Times';
+    header.appendChild(title);
+
+    const temperature = document.createElement('span');
+    temperature.classList.add('temp');
+    temperature.textContent = '98°';
+    header.appendChild(temperature);
+    
+    headerContainer.appendChild(header);
+}
+
+Header();
